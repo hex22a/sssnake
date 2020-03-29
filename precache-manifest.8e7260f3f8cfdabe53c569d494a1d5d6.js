4c56051356a24a3cf41561108fc421d4d22da007
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-29 15:45:04 +0000

    Deploy hex22a/sssnake to github.com/hex22a/sssnake.git:gh-pages

---
+++ precache-manifest.8e7260f3f8cfdabe53c569d494a1d5d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66773c32bb020550c63880d386643d9",
+    "url": "/sssnake/index.html"
+  },
+  {
+    "revision": "e7a1ca0f01c47917db10",
+    "url": "/sssnake/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "86732012c07afdd9267f",
+    "url": "/sssnake/static/js/2.40f09345.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/sssnake/static/js/2.40f09345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a1ca0f01c47917db10",
+    "url": "/sssnake/static/js/main.c49eb24b.chunk.js"
+  },
+  {
+    "revision": "155b89c1ac41e837a260",
+    "url": "/sssnake/static/js/runtime-main.0a27f3df.js"
+  }
+]);

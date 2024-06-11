42623fcf8c8927af37bd522f7ff85b5abd1ce780
Crash <1903291+hex22a@users.noreply.github.com>
2024-06-11 14:26:04 +0400

    Enable assetPrefix in next.config
    The assetPrefix has been uncommented in the next.config.mjs. This will now specify a custom asset prefix path ('/sssnake/') when in a production environment. This change improves the routing and linking of static assets for the production build.
    
    Signed-off-by: Crash <1903291+hex22a@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // assetPrefix: process.env.NODE_ENV === 'production' ? '/sssnake/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/sssnake/' : '',
     compiler: {
         styledComponents: true,
     }
